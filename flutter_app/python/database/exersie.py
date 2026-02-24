819fcd0e88658acd412db5916c59ccd5cc3325f1
Josh Aadur <joshaadur@gmail.com>
2026-02-24 06:58:47 +0000

    fixed errors

--- flutter_app/python/database/exersie.py
+++ flutter_app/python/database/exersie.py
@@ -1,12 +1,12 @@
 from login import CONN
 
-class ExersiseSelection ():
+class ExersiseSelection():
     
-    def __int__(self, CONN):
+    def __init__(self, CONN):
         self.conn = CONN
         self.cur = self.conn.cursor()
 
-#note i have got the info from database but this needs to be give to the front end somehow
+    # note i have got the info from database but this needs to be give to the front end somehow
     def exer_filter(self, name=None, area=None, type=None, equipment=None):
         
         query = """
@@ -32,7 +32,13 @@ class ExersiseSelection ():
             query += " AND e.exer_type = %s"
             params.append(type)
 
-        if equipment:  
+        if equipment:
+            # accept list of strings or list of single-value tuples from fetchall()
+            if isinstance(equipment, list) and equipment and isinstance(equipment[0], tuple):
+                equipment = [row[0] for row in equipment]
+            elif isinstance(equipment, tuple):
+                equipment = list(equipment)
+
             placeholders = ", ".join(["%s"] * len(equipment))
             query += f" AND e.exer_equip IN ({placeholders})"
             params.extend(equipment)
@@ -41,21 +47,20 @@ class ExersiseSelection ():
         
         return self.cur.fetchall()
         
-    def auto_equipment(self,user_id):
-        
+    def auto_equipment(self, user_id):
         self.cur.execute("""
 SELECT equipment_name
 FROM equipment e
 JOIN user_equip ue ON e.equip_id = ue.equip_id
 WHERE ue.user_id = %s 
-""",(user_id))
-        
-        return self.cur.fetchall()
+""", (user_id,))
+        return [row[0] for row in self.cur.fetchall()]
         
 def test():    
     plan = ExersiseSelection(CONN)
     equipment = plan.auto_equipment(2)
-    plan.exer_filter(equipment=equipment)
+    results = plan.exer_filter(equipment=equipment)
+    print(results)
     
 if __name__ == "__main__":
     test()
