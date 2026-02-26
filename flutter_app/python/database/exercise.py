6d78179c9ea7802ca1203f0e702473a55a07c88a
Josh Aadur <joshaadur@gmail.com>
2026-02-26 12:08:40 +0000

    modified exercise.py to follow api

--- flutter_app/python/database/exercise.py
+++ flutter_app/python/database/exercise.py
@@ -1,16 +1,18 @@
 from login import CONN
 
-class ExersiseSelection():
-    
-    def __init__(self, CONN):
-        self.conn = CONN
+
+class ExerciseSelection:
+
+    def __init__(self, conn=CONN):
+        self.conn = conn
         self.cur = self.conn.cursor()
 
     # note i have got the info from database but this needs to be give to the front end somehow
     def exer_filter(self, name=None, area=None, type=None, equipment=None):
-        
+
+        # Select exer_id as well so we can identify records from the frontend
         query = """
-        SELECT e.exer_name, e.exer_body_area, e.exer_type,
+        SELECT e.exer_id, e.exer_name, e.exer_body_area, e.exer_type,
             e.exer_descrip, e.exer_vid, e.exer_equip,
             pe.plan_exer_set, pe.plan_exer_amount
         FROM exersise AS e
@@ -21,8 +23,8 @@ class ExersiseSelection():
         params = []
 
         if name:
-            query += " AND e.exer_name = %s"
-            params.append(name)
+            query += " AND e.exer_name ILIKE %s"
+            params.append(f"%{name}%")
 
         if area:
             query += " AND e.exer_body_area = %s"
@@ -44,8 +46,36 @@ class ExersiseSelection():
             params.extend(equipment)
 
         self.cur.execute(query, tuple(params))
-        
-        return self.cur.fetchall()
+        rows = self.cur.fetchall()
+
+        # Map DB rows (tuples) to dictionaries matching the API schema
+        results = []
+        for row in rows:
+            # row layout: exer_id, exer_name, exer_body_area, exer_type, exer_descrip, exer_vid, exer_equip, plan_exer_set, plan_exer_amount
+            exer_id, exer_name, exer_body_area, exer_type, exer_descrip, exer_vid, exer_equip, plan_sets, plan_reps = row
+
+            # Normalize equipment into list (DB may store CSV string)
+            if exer_equip is None:
+                equipment_list = []
+            elif isinstance(exer_equip, str) and "," in exer_equip:
+                equipment_list = [e.strip() for e in exer_equip.split(",") if e.strip()]
+            elif isinstance(exer_equip, str):
+                equipment_list = [exer_equip]
+            else:
+                equipment_list = list(exer_equip)
+
+            results.append({
+                "id": exer_id,
+                "name": exer_name,
+                "body_area": exer_body_area,
+                "type": exer_type,
+                "description": exer_descrip,
+                "video_url": exer_vid,
+                "equipment": equipment_list,
+                "plan": {"sets": plan_sets, "reps": plan_reps},
+            })
+
+        return results
         
     def auto_equipment(self, user_id):
         self.cur.execute("""
