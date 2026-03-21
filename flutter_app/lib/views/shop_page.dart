3755f32428d6946fc101ccccfeba293a335375a7
Matthew Wards <belazu22@icloud.com>
2026-03-21 15:10:15 +0000

    feat: Add grid layout to shop page

--- flutter_app/lib/views/shop_page.dart
+++ flutter_app/lib/views/shop_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../data/demo_shop_items.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({Key? key}) : super(key: key);
@@ -9,34 +10,18 @@ class ShopPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Shop'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.shopping_bag,
-              size: 100,
-              color: Colors.grey[400],
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16),
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
+        children: demoShopItems.map((item) {
+          return Card(
+            child: Center(
+              child: Text(item.name),
             ),
-            const SizedBox(height: 16),
-            Text(
-              'Shop Coming Soon',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[700],
-              ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Items and equipment will be added here',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey[500],
-              ),
-            ),
-          ],
-        ),
+          );
+        }).toList(),
       ),
     );
   }
