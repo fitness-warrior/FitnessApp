b6bf4a45a8e4ca39fbce6df13fe7c73894946b71
Matthew Wards <belazu22@icloud.com>
2026-03-21 16:25:14 +0000

    feat: Create food browser page

---
+++ flutter_app/lib/views/food_browser_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../models/meal_item.dart';
+import '../data/food_db.dart';
+
+class FoodBrowserPage extends StatelessWidget {
+  const FoodBrowserPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final foods = FoodDb.all();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Food'),
+      ),
+      body: ListView.builder(
+        itemCount: foods.length,
+        itemBuilder: (context, index) {
+          final food = foods[index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundColor: _typeColor(food.type).withValues(alpha: 0.2),
+              child: Text(
+                food.type.isNotEmpty ? food.type[0] : '?',
+                style: TextStyle(
+                  color: _typeColor(food.type),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            title: Text(food.name),
+            subtitle: Text('${food.type} • ${food.calories.toInt()} kcal'),
+            trailing: const Icon(Icons.add_circle_outline),
+            onTap: () {
+              Navigator.pop(context, food);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Color _typeColor(String type) {
+    switch (type) {
+      case 'Protein':
+        return Colors.red;
+      case 'Carb':
+        return Colors.amber.shade700;
+      case 'Fat':
+        return Colors.orange;
+      case 'Vegetable':
+        return Colors.green;
+      case 'Fruit':
+        return Colors.pink;
+      case 'Dairy':
+        return Colors.lightBlue;
+      default:
+        return Colors.grey;
+    }
+  }
+}
