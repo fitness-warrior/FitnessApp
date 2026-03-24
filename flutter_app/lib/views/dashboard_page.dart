971b2d6c928b2e285832a99ca840c12369006a81
Goff120 <geoffreysmithuk@icloud.com>
2026-03-24 00:14:08 +0000

    add style of my chart's appBarr (has temp link to food)

--- flutter_app/lib/views/dashboard_page.dart
+++ flutter_app/lib/views/dashboard_page.dart
@@ -1,3 +1,6 @@
+import 'package:fitness_app_flutter/views/recipe_list_page.dart';
+import 'package:fitness_app_flutter/views/sign_up.dart';
+import 'package:fitness_app_flutter/widgets/common/header.dart';
 import 'package:flutter/material.dart';
 import '../widgets/common/navbar.dart';
 
@@ -7,13 +10,56 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard')),
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: HeaderWithDropdown(
+          title: 'My Chart',
+          onMenuSelected: (value) {
+            final route = '/${value.toLowerCase().replaceAll(' ', '_')}';
+            const routes = {'/my_workout', '/my_meal', '/shop'};
+            if (routes.contains(route)) {
+              Navigator.of(context).pushReplacementNamed(route);
+            }
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add_box),
+            tooltip: 'Add Chart',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const RecipeListPage(), //temp area 
+                ),
+              );
+            },
+          ),
+          IconButton(
+            icon: const CircleAvatar(
+              radius: 14,
+              child: Icon(Icons.person, size: 18),
+            ),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => const SignUpPage()),
+              );
+            },
+            tooltip: 'Profile',
+          ),
+        ],
+      ),
       body: const Center(
         child: Text(
           'Dashboard screen',
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
         ),
+        
       ),
+      
+
+
+      
       bottomNavigationBar: const AppBottomNavBar(currentIndex: 4),
     );
   }
