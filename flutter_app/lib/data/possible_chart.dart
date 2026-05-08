c59b68854fb1163060b9cef43d9334da5024e9db
Goff120 <geoffreysmithuk@icloud.com>
2026-05-08 15:41:45 +0100

    make structure of adding new chart page

---
+++ flutter_app/lib/data/possible_chart.dart
@@ -0,0 +1,42 @@
+import '../models/chart_model.dart';
+ 
+final demoRecipes = [
+  Chart(
+    name: 'track callories',
+    measure: [
+      'total',
+      'just intake',
+      'just cardio',
+    ],
+  ),
+  //these will get their info on if the user has 
+  //done at least one thing of the exercise (later)
+  Chart(
+    name: 'cardio speed',
+    measure: [
+      'Jump Rope',
+      'Running',
+    ],
+  ),
+   Chart(
+    name: 'cardio enduance',
+    measure: [
+      'Jump Rope',
+      'Running',
+    ],
+  ),
+   Chart(
+    name: 'total weight lifted',
+    measure: [
+      'Bench Press',
+      'Deadlift',
+    ],
+  ),
+   Chart(
+    name: 'weight personal bests',
+    measure: [
+      'Bench Press',
+      'Deadlift',
+    ],
+  ),
+];
