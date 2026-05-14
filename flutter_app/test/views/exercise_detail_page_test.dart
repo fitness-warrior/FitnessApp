fafa069731e8d053d330f23ae52fbccc41271238
Pineapples0001 <md.tanzim.hossain.2001@gmail.com>
2026-05-14 16:38:05 +0100

    Test - Video section shows when URL provided

--- flutter_app/test/views/exercise_detail_page_test.dart
+++ flutter_app/test/views/exercise_detail_page_test.dart
@@ -1,10 +1,114 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fitness_app_flutter/views/exercise_detail_page.dart';
+import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
+
+class _TestWebViewPlatform extends WebViewPlatform {
+  @override
+  PlatformNavigationDelegate createPlatformNavigationDelegate(
+    PlatformNavigationDelegateCreationParams params,
+  ) {
+    return _TestNavigationDelegate(params);
+  }
+
+  @override
+  PlatformWebViewController createPlatformWebViewController(
+    PlatformWebViewControllerCreationParams params,
+  ) {
+    return _TestWebViewController(params);
+  }
+
+  @override
+  PlatformWebViewWidget createPlatformWebViewWidget(
+    PlatformWebViewWidgetCreationParams params,
+  ) {
+    return _TestWebViewWidget(params);
+  }
+}
+
+class _TestNavigationDelegate extends PlatformNavigationDelegate {
+  _TestNavigationDelegate(super.params) : super.implementation();
+
+  @override
+  Future<void> setOnNavigationRequest(
+    NavigationRequestCallback onNavigationRequest,
+  ) async {}
+
+  @override
+  Future<void> setOnWebResourceError(
+    WebResourceErrorCallback onWebResourceError,
+  ) async {}
+
+  @override
+  Future<void> setOnPageStarted(PageEventCallback onPageStarted) async {}
+
+  @override
+  Future<void> setOnPageFinished(PageEventCallback onPageFinished) async {}
+
+  @override
+  Future<void> setOnProgress(ProgressCallback onProgress) async {}
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    return Future<void>.value();
+  }
+}
+
+class _TestWebViewController extends PlatformWebViewController {
+  _TestWebViewController(super.params) : super.implementation();
+
+  @override
+  Future<void> setJavaScriptMode(JavaScriptMode javaScriptMode) async {}
+
+  @override
+  Future<void> setPlatformNavigationDelegate(
+    covariant PlatformNavigationDelegate handler,
+  ) async {}
+
+  @override
+  Future<void> setUserAgent(String? userAgent) async {}
+
+  @override
+  Future<void> addJavaScriptChannel(
+      JavaScriptChannelParams javaScriptChannelParams) async {}
+
+  @override
+  Future<void> enableZoom(bool enabled) async {}
+
+  @override
+  Future<void> loadHtmlString(String html, {String? baseUrl}) async {}
+
+  @override
+  Future<void> setBackgroundColor(Color color) async {}
+
+  @override
+  Future<void> runJavaScript(String javaScript) async {}
+
+  @override
+  Future<Object> runJavaScriptReturningResult(String javaScript) async => '';
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    return Future<void>.value();
+  }
+}
+
+class _TestWebViewWidget extends PlatformWebViewWidget {
+  _TestWebViewWidget(super.params) : super.implementation();
+
+  @override
+  Widget build(BuildContext context) => const SizedBox.shrink();
+}
 
 Widget _wrap(Widget child) => MaterialApp(home: child);
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() {
+    WebViewPlatform.instance = _TestWebViewPlatform();
+  });
+
   group('ExerciseDetailPage', () {
     final sampleExercise = {
       'name': 'Bench Press',
@@ -119,6 +223,25 @@ void main() {
       await tester.pumpAndSettle();
 
       expect(find.text('How to do it'), findsNothing);
+    });
+
+    testWidgets('renders video section when a valid URL is provided',
+        (tester) async {
+      final exerciseWithValidVideo = {
+        'name': 'Lunge',
+        'area': 'Legs',
+        'type': 'Strength',
+        'description': 'A lower-body exercise.',
+        'equipment': ['Bodyweight'],
+        'video': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      };
+
+      await tester.pumpWidget(
+        _wrap(ExerciseDetailPage(exercise: exerciseWithValidVideo)),
+      );
+      await tester.pumpAndSettle();
+
+      expect(find.text('How to do it'), findsOneWidget);
     });
   });
 }
